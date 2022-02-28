8b9821055cae325f4bb2861dd91521dc07634df3
Vika <v.sobutska@dev-branch.com>
2022-02-28 16:06:52 +0200

    task4436 - changed JS file into drupal behaviours

--- web/themes/custom/b5subtheme/src/js/main.js
+++ web/themes/custom/b5subtheme/src/js/main.js
@@ -1,18 +1,30 @@
-// $(document).find('#block-searchiconbtn').on('click', function () {
-//     $('.search-block-form').toggleClass('active');
-// });
-
-let link = document.querySelector('#block-searchiconbtn');
-let formLink = document.querySelector('.search-block-form');
+// let link = document.querySelector('#block-searchiconbtn');
+// let formLink = document.querySelector('.search-block-form');
 let searchBtn = document.querySelector('.btn-search');
-
 searchBtn.value = 'Search >';
+//
+// link.addEventListener('click', addClass);
+//
+// function addClass(){
+//     formLink.classList.toggle('active');
+// }
 
-link.addEventListener('click', addClass);
 
-function addClass(){
-    formLink.classList.toggle('active');
-}
+/**
+ * @file
+ */
 
+(function ($) {
 
+    'use strict';
+
+    Drupal.behaviors.searchButton = {
+        attach: function (context, settings) {
+            $(context).find('#block-searchiconbtn').once('myCustomBehaviour').on('click', function () {
+           $('.search-block-form').toggle(800);
+            });
+        }
+    };
+
+})(jQuery);
 
