ee6663b4967a738648249dffed6f23d67af3f63d
Vika <v.sobutska@dev-branch.com>
2022-03-07 11:01:22 +0200

    task4474 - added test materials for Paragraph behaviour

--- web/modules/custom/paragraph_behaviors/src/Plugin/paragraphs/Behavior/GalleryBehavior.php
+++ web/modules/custom/paragraph_behaviors/src/Plugin/paragraphs/Behavior/GalleryBehavior.php
@@ -1,9 +1,11 @@
 <?php
 
-namespace Drupal\dummy\Plugin\paragraphs\Behavior;
+namespace Drupal\dlog_paragraphs\Plugin\paragraphs\Behavior;
 
+use Drupal\Component\Utility\Html;
 use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\paragraphs\Annotation\ParagraphsBehavior;
 use Drupal\paragraphs\Entity\Paragraph;
 use Drupal\paragraphs\Entity\ParagraphsType;
 use Drupal\paragraphs\ParagraphInterface;
@@ -11,42 +13,48 @@ use Drupal\paragraphs\ParagraphsBehaviorBase;
 
 /**
  * @ParagraphsBehavior(
- *   id = "dummy_image_and_text",
- *   label = @Translation("Paragraph Image and Text settings"),
- *   description = @Translation("Allows to select image size and position."),
+ *   id = "dlog_paragraphs_gallery",
+ *   label = @Translation("Gallery settings"),
+ *   description= @Translation("Settings for gallery paragraph type."),
  *   weight = 0,
  * )
  */
-class ImageAndTextBehavior extends ParagraphsBehaviorBase {
+class GalleryBehavior extends ParagraphsBehaviorBase {
 
   /**
    * {@inheritdoc}
    */
   public static function isApplicable(ParagraphsType $paragraphs_type) {
-    if ($paragraphs_type->id() == 'image_and_text') {
-      return TRUE;
-    }
-    return FALSE;
+    return $paragraphs_type->id();
   }
 
   /**
-   * {@inheritdoc}
+   * Extends the paragraph render array with behavior.
    */
   public function view(array &$build, Paragraph $paragraph, EntityViewDisplayInterface $display, $view_mode) {
-    $image_size = $paragraph->getBehaviorSetting($this->getPluginId(), 'image_size', '4_12');
-    $image_position = $paragraph->getBehaviorSetting($this->getPluginId(), 'image_position', 'left');
-    $build['#attributes']['class'][] = 'image-size--' . str_replace('_', '-', $image_size);
-    $build['#attributes']['class'][] = 'image-position--' . str_replace('_', '-', $image_position);
+    $images_per_row = $paragraph->getBehaviorSetting($this->getPluginId(), 'items_per_row', 4);
+    $bem_block = 'paragraph-' . $paragraph->bundle() . ($view_mode == 'default' ? '' : '-' . $view_mode);
+    $build['#attributes']['class'][] = Html::getClass($bem_block . '--images-per-row-' . $images_per_row);
 
-    if ($build['field_image']['#formatter'] == 'image') {
-      switch ($image_size) {
-        case '6_12':
-          $build['field_image'][0]['#image_style'] = 'image_and_text_6_of_12';
+    // @todo Image styles for different images per row.
+    if (isset($build['field_images']) && $build['field_images']['#formatter'] == 'photoswipe_field_formatter') {
+      switch ($images_per_row) {
+        case 4:
+        default:
+          $image_style = 'paragraph_gallery_image_3_of_12';
           break;
 
-        case '8_12':
-          $build['field_image'][0]['#image_style'] = 'image_and_text_8_of_12';
+        case 3:
+          $image_style = 'paragraph_gallery_image_4_of_12';
           break;
+
+        case 2:
+          $image_style = 'paragraph_gallery_image_6_of_12';
+          break;
+      }
+
+      for ($i = 0; $i < count($build['field_images']['#items']); $i++) {
+        $build['field_images'][$i]['#display_settings']['photoswipe_node_style'] = $image_style;
       }
     }
   }
@@ -55,57 +63,19 @@ class ImageAndTextBehavior extends ParagraphsBehaviorBase {
    * {@inheritdoc}
    */
   public function buildBehaviorForm(ParagraphInterface $paragraph, array &$form, FormStateInterface $form_state) {
-    $form['image_size'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Image size'),
-      '#options' => $this->getImageSizeOptions(),
-      '#default_value' => $paragraph->getBehaviorSetting($this->getPluginId(), 'image_size', '4_12'),
-    ];
 
-    $form['image_position'] = [
+    $form['items_per_row'] = [
       '#type' => 'select',
-      '#title' => $this->t('Image position'),
-      '#options' => $this->getImagePositionOptions(),
-      '#default_value' => $paragraph->getBehaviorSetting($this->getPluginId(), 'image_position', 'left'),
+      '#title' => $this->t('Number of images per row'),
+      '#options' => [
+        '2' => $this->formatPlural(2, '1 photo per row', '@count photos per row'),
+        '3' => $this->formatPlural(3, '1 photo per row', '@count photos per row'),
+        '4' => $this->formatPlural(4, '1 photo per row', '@count photos per row'),
+      ],
+      '#default_value' => $paragraph->getBehaviorSetting($this->getPluginId(), 'items_per_row', 4),
     ];
 
     return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function settingsSummary(Paragraph $paragraph) {
-    $image_size = $paragraph->getBehaviorSetting($this->getPluginId(), 'image_size', '4_12');
-    $image_size_options = $this->getImageSizeOptions();
-    $image_position = $paragraph->getBehaviorSetting($this->getPluginId(), 'image_position', 'left');
-    $image_position_options = $this->getImagePositionOptions();
-
-    $summary = [];
-    $summary[] = $this->t('Image size: @value', ['@value' => $image_size_options[$image_size]]);
-    $summary[] = $this->t('Image posiion: @value', ['@value' => $image_position_options[$image_position]]);
-    return $summary;
-  }
-
-  /**
-   * Return options for image size.
-   */
-  private function getImageSizeOptions() {
-    return [
-      '4_12' => $this->t('4 of 12'),
-      '6_12' => $this->t('6 of 12'),
-      '8_12' => $this->t('8 of 12'),
-    ];
-  }
-
-  /**
-   * Return options for image position.
-   */
-  private function getImagePositionOptions() {
-    return [
-      'left' => $this->t('Left'),
-      'right' => $this->t('Right'),
-    ];
   }
 
 }
