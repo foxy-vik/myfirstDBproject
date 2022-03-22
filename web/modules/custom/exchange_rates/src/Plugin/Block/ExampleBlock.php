4c3a0a1d207b29f3b164cff51b73bb89485591de
Vika <v.sobutska@dev-branch.com>
2022-03-22 18:38:20 +0200

    task4504 - added info about Exchange Rates eur, usd and pl

--- web/modules/custom/exchange_rates/src/Plugin/Block/ExampleBlock.php
+++ web/modules/custom/exchange_rates/src/Plugin/Block/ExampleBlock.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\exchange_rates\Plugin\Block;
 
+use Drupal;
+use Drupal\Core\Block\Annotation\Block;
 use Drupal\Core\Block\BlockBase;
 
 /**
@@ -19,8 +21,29 @@ class ExampleBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
+    $appID = '14484123d2770bca142e4f40a0b5f0a5';
+    $url = 'http://api.exchangeratesapi.io/v1/latest?access_key=' . $appID;
+    $method = 'GET';
+
+    $client = Drupal::httpClient();
+    $response = $client->request($method, $url);
+
+    $code = $response->getStatusCode();
+    if ($code == 200) {
+      $body = $response->getBody()->getContents();
+      $allRates = json_decode($body, TRUE);
+    }
+
+    $uahEUR = $allRates['rates']['UAH'];
+    $uahUSD = round($uahEUR / $allRates['rates']['USD'], 4);
+    $uahPL = round($uahEUR / $allRates['rates']['PLN'], 4);
+
+
     $build['content'] = [
-      '#markup' => $this->t('It works!'),
+      '#theme' => 'rates_block',
+      '#usd' => $uahUSD,
+      '#euro' => $uahEUR,
+      '#pl' => $uahPL,
     ];
     return $build;
   }
