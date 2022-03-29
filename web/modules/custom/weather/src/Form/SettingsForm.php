f232a8a1f2ae81ba026767ee88b7df91318b78bf
Vika <v.sobutska@dev-branch.com>
2022-03-29 14:16:01 +0300

    task4503 - added dependency injection && corrected files to codding standarts

--- web/modules/custom/weather/src/Form/SettingsForm.php
+++ web/modules/custom/weather/src/Form/SettingsForm.php
@@ -2,14 +2,54 @@
 
 namespace Drupal\weather\Form;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Exception\GuzzleException;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Configure Weather settings for this site.
  */
 class SettingsForm extends ConfigFormBase {
+
+  /**
+   * The HTTP client.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $client;
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs a new WeatherSettings instance.
+   *
+   * @param \GuzzleHttp\ClientInterface $client
+   *   The HTTP client.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   */
+  public function __construct(ClientInterface $client, ConfigFactoryInterface $config_factory) {
+    $this->client = $client;
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container): ConfigFormBase|SettingsForm|static {
+    return new static(
+      $container->get('http_client'),
+      $container->get('config.factory'),
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -32,7 +72,7 @@ class SettingsForm extends ConfigFormBase {
     $key_api_weather = $this->config('weather.settings')->get('key_weather_api');
     $url_weather = "https://api.openweathermap.org/data/2.5/weather?id=524901&lang=fr&appid=$key_api_weather";
     try {
-      $response = \Drupal::httpClient()->request('GET', $url_weather);
+      $response = $this->client->request('GET', $url_weather);
       $weather_data = json_decode($response->getBody()->getContents(), TRUE);
     }
     catch (GuzzleException $e) {
@@ -85,9 +125,6 @@ class SettingsForm extends ConfigFormBase {
     $api_key_length = strlen($form_state->getValue('api_key'));
     $city_name_for_weather = $form_state->getValue('city_weather');
     $reg_ex = "#^[A-Za-z-]+$#";
-    $test_regex = !preg_match($reg_ex, $city_name_for_weather);
-    $test_empty = empty($city_name_for_weather);
-
     if ($api_key_length != 32) {
       $form_state->setErrorByName('api_key', $this->t('The value is not correct.'));
     }
